d381cccd37041ef2653ebe9cf7f7c44c515571e2
yazlyn333 <157342381+yazlyn333@users.noreply.github.com>
2025-02-22 10:29:43 -0600

    Add files via upload

---
+++ docs/sketch.js
@@ -0,0 +1,28 @@
+const emojis = ["🩵","🏳️‍🌈","✝️","🌎","🫱🏾‍🫲🏻"];
+const numbers =  [1,2,3,4,5,6]
+function setup() {
+  createCanvas(400, 400);
+  frameRate(5)
+}
+
+function draw() {
+ const bgColor = random(["red", "yellow", "orange", "green","blue","purple"])
+ background(bgColor);
+  frameRate(5)
+for (var number of numbers){
+  
+}
+  fill("black")
+  textSize(20);
+  textStyle(BOLDITALIC);
+  text('WE ARE ALL EQUAL', 100, 200)
+  
+const emoji = random(emojis)
+const x = random (width);
+const y = random (height);
+text(emoji, x, y)
+textSize(50)
+
+{
+}
+}
